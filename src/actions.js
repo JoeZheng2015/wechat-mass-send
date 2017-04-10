8a490e09e672c287f64008193f51313512d6a36e
郑俊鑫 <18810544291@163.com>
2017-04-10 14:14:22 +0800

    add SHOW_OPTIONS

--- src/actions.js
+++ src/actions.js
@@ -1,18 +1,11 @@
 const request = require('request-promise')
 const {appendQuery} = require('./helper')
 const querystring = require('querystring')
-const {SHOW_URL} = require('./utils/constants')
+const {SHOW_URL, SHOW_OPTIONS} = require('./utils/constants')
 
 exports.fetchShows = function(url = SHOW_URL) {
-    const data = {
-        city_name: '深圳',
-        page: 1,
-        per_page: 5000,
-        type_pid: 'ff8080814a7ad6f1014a7adb6b540002',
-    }
-
     return request({
-        url: appendQuery(url, querystring.stringify(data)),
+        url: appendQuery(url, querystring.stringify(SHOW_OPTIONS)),
         json: true,
     })
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,11 @@ exports.APPID = 'wxb71708e8fc08e734'
 exports.APPSECRET = '1aba9b29319934a277ecafc7b5d338e2'
 
 exports.SHOW_URL = 'http://b2cqq.show.wepiao.com/items'
+
+exports.SHOW_OPTIONS = {
+    city_name: '深圳',
+    page: 1,
+    per_page: 5000,
+    type_pid: 'ff8080814a7ad6f1014a7adb6b540002',
+}
+

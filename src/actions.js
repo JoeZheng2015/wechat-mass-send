50c88cba088ca47f57001b8b1290c60a3cd7426e
郑俊鑫 <18810544291@163.com>
2017-04-10 11:41:47 +0800

    use stringify to encode url

--- src/actions.js
+++ src/actions.js
@@ -1,16 +1,17 @@
 const request = require('request-promise')
-const {encodeQuery} = require('./helper')
+const {appendQuery} = require('./helper')
+const querystring = require('querystring')
 
 exports.fetchShows = function(url = 'http://b2cqq.show.wepiao.com/items') {
     const data = {
-        city_name: '%E6%B7%B1%E5%9C%B3',
+        city_name: '深圳',
         page: 1,
         per_page: 5000,
         type_pid: 'ff8080814a7ad6f1014a7adb6b540002',
     }
 
     return request({
-        url: encodeQuery(url, data),
+        url: appendQuery(url, querystring.stringify(data)),
         json: true,
     })
 }
--- src/helper.js
+++ src/helper.js
@@ -1,18 +1,21 @@
-exports.encodeQuery = function (url, data = {}) {
-    if (!data || !Object.keys(data).length) {
-        return url
-    }
-
-    const result = url.indexOf('?') === -1 ? `${url}?` : `${url}&`
-    const query = Object.keys(data).map(key => `${key}=${data[key]}`).join('&')
-
-    return `${result}${query}`
-}
-
 let preShows = undefined
 exports.hasNewShow = function(shows) {
     const result = preShows !== undefined && shows.length > preShows.length
     preShows = shows
 
     return result
+}
+
+exports.notify = function(content, recervers = 0) {
+
+}
+
+exports.appendQuery = function(url, query) {
+    if (!query) {
+        return url
+    }
+
+    const result = url.indexOf('?') === -1 ? `${url}?` : `${url}&`
+
+    return `${result}${query}`
 }

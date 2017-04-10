5a736a483818e7a40a2da4b7898536f435047533
郑俊鑫 <18810544291@163.com>
2017-04-10 11:31:25 +0800

    regard url as a argument

--- src/actions.js
+++ src/actions.js
@@ -1,18 +1,16 @@
 const request = require('request-promise')
 const {encodeQuery} = require('./helper')
 
-exports.fetchShows = function () {
-    const SHOWS_URL = 'http://b2cqq.show.wepiao.com/items'
+exports.fetchShows = function(url = 'http://b2cqq.show.wepiao.com/items') {
     const data = {
         city_name: '%E6%B7%B1%E5%9C%B3',
         page: 1,
         per_page: 5000,
         type_pid: 'ff8080814a7ad6f1014a7adb6b540002',
     }
-    const url = encodeQuery(SHOWS_URL, data)
 
     return request({
-        url,
+        url: encodeQuery(url, data),
         json: true,
     })
 }

dae3c198d121d1198637119aa7987f65eaeb539d
郑俊鑫 <18810544291@163.com>
2017-04-25 19:02:26 +0800

    delete test log

--- src/app.js
+++ src/app.js
@@ -22,8 +22,5 @@ setInterval(() => {
                 const urls = newShows.map(show => `http://mqq.show.wepiao.com/detail/${show.id}?static_api=false`)
                 massSendText(`有${newShows.length}个新的演出：${urls.join(' ')}`)
             }
-            else if (new Date().getHours() === 18) {
-                massSendText('今天没有新演出了')
-            }
         })
 }, HOUR)

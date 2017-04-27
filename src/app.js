e4051d28b8714e925834418a1c03a704508c7a3f
郑俊鑫 <18810544291@163.com>
2017-04-27 14:35:08 +0800

    bugfix

--- src/app.js
+++ src/app.js
@@ -9,26 +9,6 @@ setInterval(() => {
     fetchShows()
         .then(res => {
             const shows = res.list
-console.log(shows)
-            if (preShows === undefined) {
-                preShows = shows
-            }
-            return shows
-        })
-        .then(shows => {
-            console.log(`在${new Date().getHours()}时，有${shows.length}个项目`)
-            const newShows = getNewShows(preShows, shows)
-            if (newShows.length) {
-                const urls = newShows.map(show => `http://mqq.show.wepiao.com/detail/${show.id}?static_api=false`)
-                massSendText(`有${newShows.length}个新的演出：${urls.join(' ')}`)
-            }
-        })
-}, HOUR)
-
-
-    fetchShows()
-        .then(res => {
-            const shows = res.list
             if (preShows === undefined) {
                 preShows = shows
             }
@@ -42,3 +22,6 @@ console.log(shows)
                 massSendText(`${show.item_title_cn}：http://mqq.show.wepiao.com/detail/${show.id}?static_api=false`)
             })
         })
+}, HOUR)
+
+console.log('项目已启动')
